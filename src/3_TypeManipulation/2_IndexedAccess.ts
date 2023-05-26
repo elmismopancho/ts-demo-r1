3a5814670c21059e006d8aab07569d03869ba793
Francisco Alcantara <francisco@r1vs.com>
2023-05-26 18:59:42 -0400

    type manipulation

---
+++ src/3_TypeManipulation/2_IndexedAccess.ts
@@ -0,0 +1,23 @@
+export default () => {
+  // Indexed Access
+  type Person = {
+    age: number;
+    name: string;
+    alive: boolean;
+  };
+  type Age = Person["age"];
+  //   ^?
+  const foo: Age = 10;
+  console.log(foo);
+  //          ^?
+
+  const MyArray = [
+    { name: "Alice", age: 15 },
+    { name: "Bob", age: 23 },
+    { name: "Eve", age: 38 },
+  ];
+
+  type Human = (typeof MyArray)[number];
+  let bar: Human;
+  //  ^?
+};
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,3 @@
-import n1 from "./1_Narrowing/1_AnyVsUnknown";
-import n2 from "./1_Narrowing/2_Equallity";
-import n3 from "./1_Narrowing/3_Typeof";
-import n4 from "./1_Narrowing/4_Instanceof";
-import n5 from "./1_Narrowing/5_OperatorIn";
-import n6 from "./1_Narrowing/6_OperatorIs";
-
-import g1 from "./2_Generics/1_Functions";
-import g2 from "./2_Generics/2_Classes";
-import g3 from "./2_Generics/3_Types";
-import g4 from "./2_Generics/4_Constraints";
-
-import t1 from "./3_TypeManipulation/1_KeyofTypeof";
-
-t1();
+console.log(
+  "Copy & Paste here the code from the examples, or import as a function and execute"
+);
